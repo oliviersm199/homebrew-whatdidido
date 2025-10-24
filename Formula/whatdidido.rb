502a43d206dde8607be69a7655101b060e556d61
oliviersm199 <olivier.morissette@gmail.com>
2025-10-24 14:38:37 -0400

    fix

--- Formula/whatdidido.rb
+++ Formula/whatdidido.rb
@@ -8,7 +8,6 @@ class Whatdidido < Formula
   license "MIT"
 
   depends_on "python@3.12"
-  depends_on "rust" => :build
 
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
@@ -201,9 +200,8 @@ class Whatdidido < Formula
   end
 
   def install
-    # Allow binary wheels for packages with Rust/C extensions
-    ENV["PIP_ONLY_BINARY"] = "jiter,pydantic_core,tiktoken,charset-normalizer"
-    virtualenv_install_with_resources(using: "python@3.12", link_manpages: false)
+    # Use binary wheels by default for faster installation
+    virtualenv_install_with_resources(using: "python@3.12", link_manpages: false, without_pip: false)
   end
 
   test do
